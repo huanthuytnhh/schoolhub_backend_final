19c72475b31b1691ee85d53b07bd67c2a746af6d
KhoaS84 <haikhoa71nk@gmail.com>
2025-06-11 22:30:38 +0700

    update schoolhub

---
+++ schoolhub/menu/migrations/__init__.py
@@ -0,0 +1,11 @@
+#pip install database
+#pip install django-extensions
+#pip install django-debug-toolbar
+#pip install django-redis
+# This file is intentionally left empty to mark the directory as a Python package.
+# This allows Django to recognize the migrations directory and manage migrations for the app.
+# You can add any necessary migration files in this directory.
+# If you need to create migrations, use the command:
+# python manage.py makemigrations menu
+# This will generate the initial migration file based on your models.
+# If you have existing migrations, ensure they are placed in this directory.
