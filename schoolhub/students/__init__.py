a62e0b98ffe0c34e62b25128c6dc95d73335ef6e
KhoaS84 <haikhoa71nk@gmail.com>
2025-06-11 22:32:35 +0700

    update schoolhub

---
+++ schoolhub/students/__init__.py
@@ -0,0 +1,11 @@
+#pip install -e .
+#pip install django-extensions
+# This file is intentionally left empty to mark the directory as a Python package.
+# This allows Django to recognize the students app and manage its models and views.
+# You can add any necessary models, views, or other files in this directory.
+# If you need to create models, use the command:
+# python manage.py startapp students
+# This will generate the initial structure for the students app.
+# If you have existing models, ensure they are placed in this directory.
+# This file is intentionally left empty to mark the directory as a Python package.
+# This allows Django to recognize the students app and manage its models and views.
